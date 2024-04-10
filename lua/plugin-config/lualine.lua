853cc621e3ce2a1307b30e0419fb232625642464
maybe0828 <51359732+maybe0828@users.noreply.github.com>
2024-04-10 17:47:43 +0800

    [2024/04/10]Update lualine.lua

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -21,30 +21,69 @@ lualine.setup({
         lualine_a = {"mode"},
         --lualine_b = {"branch", "diff", "diagnostics"},
         lualine_b = {
-            "filename",
-            "diff",
+            "branch",
+        },
+        lualine_c = {
+            {
+                "filename",
+                file_status = true,
+                path = 0,
+                symbols = {
+                    modified = '[M]',      -- Text to show when the file is modified.
+                    readonly = '[R]',      -- Text to show when the file is non-modifiable or readonly.
+                    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                    newfile = '[New]',     -- Text to show for newly created file before first write
+                },
+            },
+            {
+                "diff",
+                colored = true,
+                symbols = { -- Changes the symbols used by the diff.
+                    added = '+',
+                    modified = '~',
+                    removed = '-'
+                },
+            },
             {
                 "lsp_progress",
                 spinner_symbols = { "🌑 ", "🌒 ", "🌓 ", "🌔 ", "🌕 ", "🌖 ", "🌗 ", "🌘 " },
             },
         },
-        lualine_c = {
-            "filetype",
-        },
         lualine_x = {
-            "aerial",
             {
                 "fileformat",
                 symbols = {
                     unix = "unix",
                     dos = "dos",
                     mac = "mac",
+                    --unix = '', -- e712
+                    --dos = '',  -- e70f
+                    --mac = '',  -- e711
                 },
             },
             "encoding",
         },
-        lualine_y = {"progress"},
-        lualine_z = {"location"},
+        lualine_y = {
+            {
+                "filetype",
+                icon_only = false,
+            },
+            "filesize",
+        },
+        lualine_z = {
+            "progress",
+            "location",
+        },
+    },
+    winbar = {
+        lualine_a = {
+            "aerial"
+        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
     },
     extensions = {
         "nvim-tree"
