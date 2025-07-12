3dfc57ad1a9a649d52f7bbe6ac4716b91a139909
maybe0828 <51359732+maybe0828@users.noreply.github.com>
2025-07-12 13:22:10 +0800

    [2025/07/12]Update telescope.lua

--- lua/plugin-config/telescope.lua
+++ lua/plugin-config/telescope.lua
@@ -10,13 +10,29 @@ telescope.setup({
         -- 打开弹窗后进入的初始模式，默认为 insert，也可以是 normal
         initial_mode = "insert",
         -- vertical , center , cursor
-        layout_strategy = "horizontal",
+        --layout_strategy = "horizontal",
+        layout_strategy = "vertical",
+        wrap_results = true,
         -- 窗口内快捷键
         mappings = require("keymaps").telescopeList,
+        --path_display = {
+        --    "smart",
+        --},
+        preview = true,
     },
     pickers = {
         find_files = {
             -- theme = "dropdown", -- 可选参数： dropdown, cursor, ivy
+            layout_config = {
+                width = 0.95, -- 增大窗口宽度(默认0.8)
+                height = 0.95, -- 增大窗口高度(默认0.8)
+            },
+        },
+        live_grep = {
+            layout_config = {
+                width = 0.95, -- 增大窗口宽度(默认0.8)
+                height = 0.95, -- 增大窗口高度(默认0.8)
+            }.
         },
     },
 })
